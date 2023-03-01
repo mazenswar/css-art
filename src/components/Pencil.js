c4bea8875d30528198451d45376a1ab1768d5725
Mazen Al Swar <43959520+mazenswar@users.noreply.github.com>
2023-03-01 13:23:13 -0500

    pencil

---
+++ src/components/Pencil.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Pencil = () => {
+	return (
+		<div className="pencil">
+			<div className="eraser"></div>
+			<div className="pencil-body"></div>
+			<div className="tip">
+				<div className="pen-tip"></div>
+			</div>
+		</div>
+	);
+};
+
+export default Pencil;
